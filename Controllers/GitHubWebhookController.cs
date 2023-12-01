0d74868b29c213337c9b052cdab3fb036e640d95
ChrisMarSilva <chris.mar.silva@gmail.com>
2023-12-01 17:37:59 +0000

    ajustes

---
+++ Controllers/GitHubWebhookController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CMS_DotNet_Teste_WebAPI_WebHooks.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class GitHubWebhookController : ControllerBase
+{
+    //[GitHubWebHook]
+    //public IActionResult GitHubHandler(JObject data)
+    //{
+    //    var result = JsonConvert.SerializeObject(data);
+
+    //    return Ok();
+    //}
+
+    [HttpPost]
+    public IActionResult Pushed(dynamic payload)
+    {
+        return Ok();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,18 +3,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddGitHubWebHooks();
+//builder.Services.AddGitHubWebHooks();
+//builder.Services.AddOptions();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseMvcWithDefaultRoute();
+//app.UseMvcWithDefaultRoute();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
